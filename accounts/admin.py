539f734347b3bbd9bd182ac6bcb5052cc73ca0ec
Julio Cesar <julio-cesaar21@hotmail.com>
2025-02-05 02:30:53 -0300

    novos bus

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,15 +1,38 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-
-from .forms import CustomUserCreationForm, CusterUserChangeForm
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import CustomUser
 
-class CustomClassAdmin(UserAdmin):
+class CustomUserAdmin(UserAdmin):
+    # Formulários personalizados para criação e edição de usuários
     add_form = CustomUserCreationForm
-    form = CustomUserCreationForm
+    form = CustomUserChangeForm
     model = CustomUser
-    list_display = ["email", "username",]
 
+    # Campos a serem exibidos na lista de usuários
+    list_display = ('username', 'email', 'is_verified', 'is_staff', 'created_at')
+    
+    # Campos a serem usados nos filtros do admin
+    list_filter = ('is_verified', 'is_staff')
 
-admin.site.register(CustomUser, CustomClassAdmin)
+    # Campos para pesquisa no admin
+    search_fields = ('username', 'email')
 
+    # Campos a serem exibidos no formulário de edição
+    fieldsets = (
+        (None, {'fields': ('username', 'email', 'password')}),
+        ('Informações Pessoais', {'fields': ('telefone', 'data_nascimento')}),
+        ('Permissões', {'fields': ('is_verified', 'is_staff', 'is_active', 'groups', 'user_permissions')}),
+        ('Datas', {'fields': ('created_at',)}),
+    )
+
+    # Campos a serem exibidos no formulário de criação
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2'),
+        }),
+    )
+
+# Registrando o CustomUser no admin
+admin.site.register(CustomUser, CustomUserAdmin)
--- accounts/forms.py
+++ accounts/forms.py
@@ -2,13 +2,11 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import CustomUser
 
 class CustomUserCreationForm(UserCreationForm):
-
     class Meta:
         model = CustomUser
         fields = ("username", "email")
 
-class CusterUserChangeForm(UserChangeForm):
-
+class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
         fields = ("username", "email")
