efd373a3184e0fb982d73b06cb1161a81e52e004
Julio Cesar <julio-cesaar21@hotmail.com>
2025-02-06 02:17:55 -0300

    account e api feitas

--- account/urls.py
+++ account/urls.py
@@ -1,10 +1,15 @@
-from django.urls import path, include
 from django.contrib import admin
+from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import RegisterView
+from account.views import RegisterView
+from .views import LogoutView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
-    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path("login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("logout/", LogoutView.as_view(), name="logout"),
+
 ]
