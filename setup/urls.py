257f8b1e33dc79c45d0fc88ab3dc689b679c6a44
Julio Cesar <julio-cesaar21@hotmail.com>
2025-02-06 01:50:11 -0300

    account e api feitas

--- setup/urls.py
+++ setup/urls.py
@@ -6,6 +6,5 @@ from account.views import RegisterView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('galeria.urls')),
-    path('admin/', admin.site.urls),
     path('api/account/', include('account.urls'))
 ]
