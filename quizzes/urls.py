1986f90d80eb8fdeb8cc8ddda52683b64187cc98
Julio Cesar <julio-cesaar21@hotmail.com>
2025-02-08 02:20:06 -0300

    todo backend e API finalizadas

--- quizzes/urls.py
+++ quizzes/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import QuizViewSet, home
+from .views import QuizViewSet, home, jogar_quiz, iniciar_quiz
 
 router = DefaultRouter()
 router.register(r'quizzes', QuizViewSet)
@@ -9,5 +9,6 @@ urlpatterns = [
     path('', home, name='home'),
     path('', include(router.urls)),
     path('create/', QuizViewSet.as_view({'post': 'create'}), name='quiz_create'),
-    path('play/<int:quiz_id>/', QuizViewSet.as_view({'get': 'retrieve'}), name='quiz_play'),
+    path('play/<int:quiz_id>/', jogar_quiz, name='quiz_play'),
+    path('iniciar/<int:sala_id>/', iniciar_quiz, name='iniciar_quiz'),
 ]
